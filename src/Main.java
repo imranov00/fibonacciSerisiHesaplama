e6cd022df5738bbadb5409afb9c83286f3d30c62
İmranov <semtexpert@gmail.com>
2023-12-28 14:34:20 +0300

    versiyon 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Fibonacci Serisinin E,leman sayısını Giriniz: ");
+        int n = input.nextInt();
+
+
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int toplam = a+b;
+            a=b;
+            b= toplam;
+
+        }
     }
 }
